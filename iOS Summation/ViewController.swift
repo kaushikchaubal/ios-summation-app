70c6ae6d05baed0589627526047d03544ec0b992
Kaushik Chaubal <kaushikchaubal@gmail.com>
2015-01-31 16:43:09 +0000

    Initial Commit

---
+++ iOS Summation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Summation
+//
+//  Created by Kaushik Chaubal on 31/01/2015.
+//  Copyright (c) 2015 Kaushik Chaubal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
